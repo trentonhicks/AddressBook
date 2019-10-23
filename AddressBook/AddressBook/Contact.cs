bc3cf0ea226404b03e21496b94c88d9212f73d3b
Trenton <trentonmhicks@gmail.com>
2019-10-23 07:56:34 -0700

    Started working on Contact class

--- AddressBook/AddressBook/Contact.cs
+++ AddressBook/AddressBook/Contact.cs
@@ -6,5 +6,28 @@ namespace AddressBook
 {
     class Contact
     {
+        // ID
+        private int _id;
+        public int ID
+        {
+            get => _id != 0 ? _id : -1;
+            set => _id = value;
+        }
+
+        // First Name
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName != "" ? _firstName : "NULL";
+            set => _firstName = value;
+        }
+
+        // Last Name
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName != "" ? _lastName : "NULL";
+            set => _lastName = value;
+        }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,7 +6,7 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
