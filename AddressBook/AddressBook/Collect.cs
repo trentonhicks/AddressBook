e992db2f98153ca462e6b97abc910b673c08733c
unknown <plwieseler@gmail.com>
2019-10-23 10:04:57 -0500

    Started working on Collect class.

--- AddressBook/AddressBook/Collect.cs
+++ AddressBook/AddressBook/Collect.cs
@@ -6,5 +6,38 @@ namespace AddressBook
 {
     class Collect
     {
+        /// <summary>
+        /// Passes in information from the user and re
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public string CollectField(string fieldName, bool required)
+        {
+            var userInput = "";
+            if (required)
+            {
+                while (true)
+                {
+                    Console.ReadLine();
+                    if (userInput == "")
+                    {
+                        Console.WriteLine($"{fieldName} is required");
+                    }
+                    //If input is dectected in the required field than break the while loop. 
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+
+            }
+            return userInput;
+        }
+
+
     }
 }
