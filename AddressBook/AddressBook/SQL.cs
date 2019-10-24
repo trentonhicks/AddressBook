0ee5646ea64a98342f1dc0bd19bd21e9b9a8ce8c
Trenton <trentonmhicks@gmail.com>
2019-10-23 18:42:39 -0700

    Added methods to get contact info in SQL.cs

--- AddressBook/AddressBook/SQL.cs
+++ AddressBook/AddressBook/SQL.cs
@@ -9,6 +9,53 @@ namespace AddressBook
     class SQL
     {
         // SQL connection string
-        private static SqlConnection _connectionString = new SqlConnection(@"Data Source=.; Initial Catalog=AddressBook2.0; Integrated Security=SSPI;");
+        private static SqlConnection _connection = new SqlConnection(@"Data Source=.; Initial Catalog=AddressBook2.0; Integrated Security=SSPI;");
+
+        // Get Contacts
+        public List<Contact> GetContacts()
+        {
+            // Store db contacts to list
+            var contacts = new List<Contact>();
+
+            _connection.Open();
+
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = @"SELECT ID, FirstName, LastName, Contacts";
+                command.CommandType = CommandType.Text;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    // Loop through all contacts in the database
+                    while (reader.Read())
+                    {
+                        // Create contact
+                        var contact = new Contact()
+                        {
+                            ID = (int)reader["ID"],
+                            FirstName = (string)reader["FirstName"],
+                            LastName = (string)reader["LastName"]
+                        };
+                        // Add contact to list
+                        contacts.Add(contact);
+                    }
+                }
+            }
+            _connection.Close();
+            return contacts;
+        }
+        /// <summary>
+        /// Display list of contacts from the database
+        /// </summary>
+        public void DisplayContactsList()
+        {
+            var contacts = GetContacts();
+
+            foreach(var contact in contacts)
+            {
+                Console.WriteLine($"{contact.ID} {contact.FirstName} {contact.LastName}");
+            }
+        }
+
     }
 }
