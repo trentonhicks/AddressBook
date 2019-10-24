29ee13e301a32c857866285f7f4eae97264cdfa8
Evan Nettleton <evangnettleton@gmail.com>
2019-10-23 20:42:32 -0500

    Added Email and PhoneNumber class

---
+++ AddressBook/AddressBook/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Email
+    {
+        private string _text;
+        public string Text
+        {
+            get => _text != "" ? _text : "NULL";
+            set => _text = value;
+        }
+
+        private string _type;
+        public string Type
+        {
+            get => _type != "" ? _type : "NULL";
+            set => _type = value;
+        }
+    }
+}
