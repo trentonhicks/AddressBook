cae0cd9f5f45c4c86e962cde8af1f4d10dea3113
Trenton <trentonmhicks@gmail.com>
2019-10-23 17:15:11 -0700

    Added SQL connection string

--- AddressBook/AddressBook/SQL.cs
+++ AddressBook/AddressBook/SQL.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace AddressBook
 {
     class SQL
     {
+        // SQL connection string
+        private static SqlConnection _connectionString = new SqlConnection(@"Data Source=.; Initial Catalog=AddressBook2.0; Integrated Security=SSPI;");
     }
 }
